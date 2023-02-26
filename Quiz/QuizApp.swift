4026c7092913b64f7da7e4c64d238ef7ae4aead2
juliwhite <julietawhite30@gmail.com>
2023-02-25 20:26:46 -0700

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Julieta White on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
